beb398f35d055ab54f3986a9ac6da88525bb24e2
wlsh44 <k87913j@gmail.com>
2026-03-16 10:22:08 +0900

    프로젝트 세팅

---
+++ src/main/java/wlsh/project/intervai/IntervaiApplication.java
@@ -0,0 +1,13 @@
+package wlsh.project.intervai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntervaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntervaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wlsh/project/intervai/IntervaiApplicationTests.java
@@ -0,0 +1,13 @@
+package wlsh.project.intervai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntervaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
